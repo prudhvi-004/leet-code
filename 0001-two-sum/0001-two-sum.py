e42becb2239268e39ec65b1e06594585c663d729
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-05-13 07:42:32 +0530

    Time: 0 ms (100%), Space: 13.3 MB (30.93%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
