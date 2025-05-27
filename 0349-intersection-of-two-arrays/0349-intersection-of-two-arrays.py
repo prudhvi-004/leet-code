f0957494f8e0f2a33915f2d3c36784218bfc3368
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-05-27 10:21:01 +0530

    Time: 1 ms (45.7%), Space: 12.6 MB (57.69%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,5 +1,25 @@
-__import__('atexit').register(lambda: open('display_runtime.txt','w').write('0'))
-
 class Solution(object):
-    def intersection(self, nums1, nums2):
-        return list(set(nums1) & set(nums2))
+    def intersection(self, num1, num2):
+        num1.sort()
+        num2.sort()
+
+        n1 = len(num1)
+        n2 = len(num2)
+
+        i = 0
+        j = 0
+
+        result = []
+
+        while i<n1 and j<n2:
+            if num1[i] == num2[j]:
+                if not result or result[-1]!=num1[i]:
+                    result.append(num1[i])
+                i+=1
+                j+=1
+            elif num1[i]<num2[j]:
+                i+=1
+            else:
+                j+=1
+        
+        return result
