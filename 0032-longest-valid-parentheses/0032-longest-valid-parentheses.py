388750df57abe28e5fc4ccfe23c3fb4d3e75e966
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-05-28 14:13:20 +0530

    Time: 11 ms (57.55%), Space: 12.9 MB (73.92%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        max_len = 0
+        left = right = 0
+
+        # Left to right
+        for i in range(len(s)):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                max_len = max(max_len, 2 * right)
+            elif right > left:
+                left = right = 0
+
+        # Right to left
+        left = right = 0
+        for i in range(len(s)-1, -1, -1):
+            if s[i] == ')':
+                right += 1
+            else:
+                left += 1
+            if left == right:
+                max_len = max(max_len, 2 * left)
+            elif left > right:
+                left = right = 0
+
+        return max_len
