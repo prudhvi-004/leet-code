9f41cbdfb30814b4b0d08560ab0e6c15c02d3433
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-06-03 08:52:14 +0530

    Time: 8 ms (48.61%), Space: 13.7 MB (35.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -2,9 +2,10 @@ class Solution(object):
     def moveZeroes(self, arr):
         """
         :type arr: List[int]
-        :rtype: None Do not return anything, modify arr in-place instead.
+        :rtype: None (modifies arr in-place)
         """
-        pos = 0
+        pos = 0  
+
         for i in range(len(arr)):
             if arr[i] != 0:
                 arr[pos], arr[i] = arr[i], arr[pos]
