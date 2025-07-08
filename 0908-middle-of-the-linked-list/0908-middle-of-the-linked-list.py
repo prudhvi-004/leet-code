bfc9f3112bb789ac410d13d0ea73af06fc987c7e
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-07-08 13:24:22 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.28%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def middleNode(self, head):
-        # Step 1: Count total number of nodes
+        #Count total number of nodes
         count = 0
         current = head
         while current:
             count += 1
             current = current.next
         
-        # Step 2: Find middle position
+        #Find middle position
         mid_index = count // 2
         
-        # Step 3: Traverse again to reach the middle node using a for loop
+        #Traverse again to reach the middle node using a for loop
         current = head
         for _ in range(mid_index):
             current = current.next
