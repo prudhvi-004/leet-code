2beaa1cf3d5e8fae4a44ca5bd404a96940d0dffa
prudhvi-004 <prudhvi.bablu04@gmail.com>
2025-07-09 13:01:03 +0530

    Time: 25 ms (12.75%), Space: 12.5 MB (7.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,23 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x == 0:
-            return 0
-
-        num = ""
-        neg = x < 0
+        sign = -1 if x < 0 else 1
         x = abs(x)
         
-        while x != 0:
-            quot = x % 10
-            num += str(quot)
-            x = x // 10
-        
-        result = int(num)
-        if neg:
-            result = -result
+        reversed_x = int(str(x)[::-1])
+        result = sign * reversed_x
 
+        # Check for 32-bit integer overflow
         if result < -2**31 or result > 2**31 - 1:
             return 0
-
         return result
